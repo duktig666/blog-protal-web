2344f75f1aea3c45b13dad9d803442efc5914749
rsw <1487660836@qq.com>
2020-05-05 12:24:38 +0800

    侧边栏完成 rsw

--- js/aside.js
+++ js/aside.js
@@ -1,21 +1,56 @@
+const loveStartTime = "2018-01-15 23:00:00";
+
 $(document).ready(function () {
     //侧边栏的vue
     let aside = new Vue({
         el: '#aside-wrap',
         data: {
+            //博主信息
             bloggerInfo: '',
-        },
-        methods: {
-            //查询博主信息
-            getBloggerInfo() {
-                $.get(baseUrl + "/api/user/8", function (data, status, xhr) {
-                    this.bloggerInfo=data;
-                    console.log(this.bloggerInfo)
-                },"json");
-            },
-        },
-        mounted() {
-            this.getBloggerInfo();
+            //最新博客信息
+            blogNewInfos: '',
+            //最热博客信息
+            blogHotInfos: '',
+            //博客的总访问量、点赞量、评论量信息
+            blogCount:'',
         }
     });
+
+    //博客的总访问量、点赞量、评论量显示
+    $.get(baseUrl + "/api/blog/count", function (data, status, xhr) {
+        aside.blogCount = data;
+    }, "json");
+
+    //情侣小空间时间计算
+    setInterval(function () {
+        let loveTime = timeLag(loveStartTime);
+        $('.love-time').text("相恋" + loveTime);
+    }, 1000);
+
+    //请求博主信息
+    $.get(baseUrl + "/api/user/8", function (data, status, xhr) {
+        aside.bloggerInfo = data;
+    }, "json");
+
+    //请求博客信息
+    function getBlogInfoNewOrHot(isNew) {
+        $.get(baseUrl + "/api/blog/all",
+            {
+                currentPage: 0,
+                rows: 10,
+                sort: isNew ? 'create_date desc' : 'visit_number desc'
+            },
+            function (data, status, xhr) {
+                if (isNew) {
+                    aside.blogNewInfos = data.items;
+                } else {
+                    aside.blogHotInfos = data.items;
+                }
+            }, "json");
+    }
+    //请求最新博客信息
+    getBlogInfoNewOrHot(true);
+    //请求最热博客信息
+    getBlogInfoNewOrHot(false);
+
 });
--- js/common.js
+++ js/common.js
@@ -1,4 +1,30 @@
-
 //项目后台请求基础路径
-const baseUrl="http://127.0.0.1:8090";
+const baseUrl = "http://127.0.0.1:8090";
+
+//计算某时间距离现在的时差（天、时、分、秒）
+function timeLag(oldTime) {//di作为一个变量传进来
+    //如果时间格式是正确的，那下面这一步转化时间格式就可以不用了
+    //将-转化为/，使用new Date
+    let dateBegin = new Date(oldTime.replace(/-/g, "/"));
+    //获取当前时间
+    let newDate = new Date();
+    //时间差的毫秒数
+    let dateDiff = newDate.getTime() - dateBegin.getTime();
+    //计算出相差天数
+    let dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000));
+    //计算天数后剩余的毫秒数
+    let leave1 = dateDiff % (24 * 3600 * 1000);
+    //计算出小时数
+    let hours = Math.floor(leave1 / (3600 * 1000));
+    //计算小时数后剩余的毫秒数
+    let leave2 = leave1 % (3600 * 1000);
+    //计算相差分钟数
+    let minutes = Math.floor(leave2 / (60 * 1000));
+    //计算分钟数后剩余的毫秒数
+    let leave3 = leave2 % (60 * 1000);
+    //计算相差秒数
+    let seconds = Math.floor(leave3 / 1000);
+    return " "+dayDiff + " 天 " + hours + " 时 " + minutes + " 分 " + seconds + " 秒 ";
+}
+
 
