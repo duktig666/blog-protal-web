cc8dde9d142cb99a844ccfa4b01e70eacb0b8c11
qianjin <1542890862@qq.com>
2020-05-05 21:29:01 +0800

    博客模块完成 JQJ

--- js/blog.js
+++ js/blog.js
@@ -3,29 +3,29 @@ Vue.component('blog-message',{
     template:`<div>
                 <div class='blog-content' v-for="item in items">
                     <div>
-                        <div class='title'><span>{{item.blog.title}}</span></div>
-                        <p>{{item.blog.summary}}</p>
+                        <div class='title'><span>{{item.title}}</span></div>
+                        <p class="txt">{{item.summary}}</p>
                     </div>
                     <div class='line'></div>
                     <div class='num'>
                         <!--  留言量 -->
-                        <span v-test=''>
+                        <span>
                             <i class='el-icon-chat-dot-round'></i>
-                            {{item.blog.observeNumber}}
+                            {{item.observeNumber}}
                         </span>
                         <!--  点赞量 -->
                         <span>
                             <i class='el-icon-star-off'></i>
-                            {{item.blog.likeNumber}}
+                            {{item.likeNumber}}
                         </span>
                         <!--  访问量 -->
                         <span>
                             <i class='el-icon-view'></i>
-                            {{item.blog.visitNumber}}
+                            {{item.visitNumber}}
                         </span>
                         <!--  发布时间 -->
                         <span>
-                            <span>发布时间：{{item.blog.createDate}}</span>
+                            <span>发布时间：{{item.createDate}}</span>
                         </span>
                         <span class='more'>
                             <el-button type='primary' size='mini'>查看更多</el-button>
--- js/home.js
+++ js/home.js
@@ -7,8 +7,9 @@ $(document).ready(function () {
                 {url:'../img/cloud.png'},
                 {url:'../img/code.jpg'},
                 {url:'../img/sun.png'},
-                {url:'../img/background-grobal.jpg'},
-            ]
+                {url:'../img/background-global.jpg'},
+            ],
+            totalSize: 0,// 总条数
         }
     });
     // 博客信息渲染
@@ -17,18 +18,55 @@ $(document).ready(function () {
         data(){
             return({//使用data返回数据，使其只改变当前组件的信息，而不是改变全局的信息
                 items:[],
+                totalSize: 0,// 总条数
+                //分页信息
+                pageInfo: {
+                    currentPage: 1, //当前页
+                    rows: 10, //每页大小
+                    sort: "", //排序方式
+                },
             })
         },
         mounted() {
           this.getBlogs();
         },
+        watch: {
+            pageInfo: { // 监视pagination属性的变化
+                deep: true, // deep为true，会监视pagination的属性及属性中的对象属性变化
+                handler() {
+                    // 变化后的回调函数，这里我们再次调用getDataFromServer即可
+                    this.getBlogs();
+                }
+            },
+        },
         methods: {
             getBlogs() {
-                $.get(baseUrl + "/api/blog/all", function (data, status, xhr) {
-                    blogList.items=data.items;
-                    console.log(blogList.items)
-                },"json");
-            }
-        }
+                $.ajax({
+                    url: ""+baseUrl+"/api/blog/all/single",
+                    dataType:"json",
+                    type:"get",
+                    data:{
+                        "currentPage": this.pageInfo.currentPage,// 当前页
+                        "rows": this.pageInfo.rows,// 每页大小
+                        "sort": null,// 排序字段
+                        blogDimSearchStr: null, // 博客模糊查询所需数据
+                    },
+                    success:function(data) {
+                        blogList.items=data.items;
+                        blogList.totalSize = data.total;
+                        console.log(blogList.items)
+                    }
+                })
+            },
+            //当前条数改变
+            handleSizeChange(val) {
+                this.pageInfo.rows = val;
+            },
+            //当前页改变
+            handleCurrentChange(val) {
+                this.pageInfo.currentPage = val;
+            },
+        },
+
     })
 })
